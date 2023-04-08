c08f6c2919ceed05f9895ea382fd03f3191e7011
Katy Rosli <katyrosli@gmail.com>
2023-04-08 11:49:53 +0200

    fixed home page

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -1,12 +1,21 @@
+import Image from 'next/image';
+import ProfilePhoto from './assets/ProfilePhoto.svg';
+
 const Summary = () => {
-    return (
-        <div className='justify-between mx-auto lg:max-w-7xl md:flex md:px-48 mb-16'>
-            <p className='text-base text-left-align'>I’m a Frontend Developer who is passionate about programming with TypeScript and 
-                JavaScript and creating beautiful and responsive user interfaces. I love to learn 
-                and stay up to date with the latest technologies. Outside of work, I enjoy skiing 
-                and kayaking as hobbies. I live in Stockholm with my family and I’m excited to 
-                share my knowledge and insights on my blog.</p>
-        </div>
-    )
+  return (
+    <div className='max-w-7xl w-full lg:max-w-full mx-auto lg:flex border border-gray-400 rounded-md'>
+      <div className='h-46'>
+        <Image src={ProfilePhoto} alt='profile photo'/>
+      </div>
+      <div className="p-2 flex flex-col justify-between leading-normal max-w-7xl">
+        <p className="text-base mb-0 my-1 ml-2">I’m a Frontend Developer who is passionate about programming with TypeScript and
+          JavaScript to create beautiful responsive web applications. I love to learn 
+          and stay up to date with the latest technologies. This blog is about me sharing what my passion about programming.
+          Outside of work, I enjoy skiing and kayaking as my hobbies. I live in Stockholm, Sweden with my husband and my son 
+          and I’m excited to share my knowledge and insights on my blog.</p>
+      </div>
+    </div>
+  )
 };
+
 export default Summary;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Summary from '@/components/Summary';
 export default function Home() {
   return (
     <Layout>
-      <div className='justify-between px-16 mx-auto lg:max-w-7xl md:flex md:px-48 mb-40'>
+      <div className='justify-between mx-auto lg:max-w-7xl md:flex md:px-48 mb-40'>
         <div className='mb-8'>
           <h1 className='font-bold text-5xl'>Hi,<br/>I’m Katy Rosli.</h1>
           <h2>Frontend Developer, with Backend knowledge.</h2>
@@ -16,7 +16,9 @@ export default function Home() {
           <Link href='/blog'><button className="border-2 border-violet-700 hover:bg-violet-900 hover:text-white text-violet-700 font-bold py-2 px-4 rounded-full">Read the Blog</button></Link>
         </div>
       </div>
-      <Summary />
+      <div className='justify-between mx-auto lg:max-w-7xl md:flex md:px-48 mb-40'>
+        <Summary />
+      </div>
     </Layout>
   )
 };
