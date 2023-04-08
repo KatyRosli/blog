1cb2e97f346a35e64a2101b4f22015f2f630b2cb
Katy Rosli <katyrosli@gmail.com>
2023-04-08 19:54:10 +0200

    edit about me

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import ProfilePhoto from './assets/ProfilePhoto.svg';
+import ProfilePhoto from './assets/ProfilePhoto.png';
 
 const Summary = () => {
   return (
@@ -9,10 +9,10 @@ const Summary = () => {
       </div>
       <div className="p-2 flex flex-col justify-between leading-normal max-w-7xl">
         <p className="text-base mb-0 my-1 ml-2">I’m a Frontend Developer who is passionate about programming with TypeScript and
-          JavaScript to create beautiful responsive web applications. I love to learn 
-          and stay up to date with the latest technologies. This blog is about me sharing what my passion about programming.
-          Outside of work, I enjoy skiing and kayaking as my hobbies. I live in Stockholm, Sweden with my husband and my son 
-          and I’m excited to share my knowledge and insights on my blog.</p>
+          JavaScript to create beautiful responsive web applications. I love learning
+          and stay up to date with the latest technologies. Outside of work, I enjoy skiing and kayaking as my hobbies. 
+          I live in Stockholm (Sweden) with my husband and my son. This blog is about me sharing my passion about programming
+          and with that I’m excited to share my knowledge and insights here on my blog.</p>
       </div>
     </div>
   )
