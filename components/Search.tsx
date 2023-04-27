f7f5640a74c0e24dfe36eed05869b90bf99cd380
Katy Rosli <katyrosli@gmail.com>
2023-04-27 10:29:06 +0200

    search function works

--- components/Search.tsx
+++ components/Search.tsx
@@ -3,11 +3,15 @@ import { useState } from "react";
 const Search = (props: { setSearchValue: (arg0: string) => void; value: string | number | readonly string[] | undefined;}) => {
   const [searchValue, setSearchValue] = useState('');
 
-  const onKeyDownHandler = (event: { key: string; }) => {
+  const onKeyUpHandler = (event: { key: string; }) => {
+    if (searchValue.length === 0) {
+      props.setSearchValue('');
+    }
+  }
+
+  const onEnterKeyDownHandler = (event: { key: string; }) => {
     if (event.key === 'Enter' && searchValue && searchValue.length > 0) {
       props.setSearchValue(searchValue);
-    } else if (searchValue.length === 0) {
-        props.setSearchValue(searchValue);
     }
   }
 
@@ -20,7 +24,8 @@ const Search = (props: { setSearchValue: (arg0: string) => void; value: string |
             value={searchValue}
             className='search-input relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-full border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary'
             onChange={(event) => setSearchValue(event.target.value)}
-            onKeyDown={onKeyDownHandler}
+            onKeyUp={onKeyUpHandler}
+            onKeyDown={onEnterKeyDownHandler}
             placeholder='Search blogpost...'
           />
         </div>
@@ -30,3 +35,4 @@ const Search = (props: { setSearchValue: (arg0: string) => void; value: string |
 };
 
 export default Search;
+
