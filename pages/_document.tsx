0bc9ca687e5a15bb78afb93f4e836598886e90f8
Katy Rosli <katyrosli@gmail.com>
2023-05-04 18:48:45 +0200

    added descripton

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
     <Html>
       <Head>
-        <meta name='description' content='CodeWithKatyRosli' />
+        <meta name='Katy Rosli a Frontend Developer who is passionate about programming with TypeScript and JavaScript' content='CodeWithKatyRosli' />
         <link rel='icon' href='/Favicon.svg' />
         <link rel='apple-touch-icon'  sizes='192x192' href='logo192.svg' />
         <link rel='apple-touch-icon'  sizes='512x512' href='logo512.svg' />
