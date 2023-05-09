ab8be43129d8a6a459773c7c9645fec7373ca28a
Katy Rosli <katyrosli@gmail.com>
2023-05-09 15:43:10 +0200

    style react markdown

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-children-prop */
 import Layout from '@/components/Layout';
 import { BlogEntry } from '../../lib/types';
 import { fetcher } from '../../lib/api';
@@ -6,6 +7,17 @@ import rehypeRaw from 'rehype-raw';
 import rehypeHighlight from 'rehype-highlight'
 import { NextSeo } from 'next-seo';
 import { ImageUrl } from '@/utils';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import {
+    materialDark,
+    materialLight,
+    oneLight,
+  } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { CSSProperties, ReactElement } from 'react';
+import PrismJsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx'
+import markdown from 'react-syntax-highlighter/dist/cjs/languages/prism/markdown'
+import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx';
+
 
 type Props = {
     blog: BlogEntry,
@@ -14,7 +26,17 @@ type Props = {
     nextBlog?: BlogEntry,
 }
 
+type CodeProps = {
+    node: ReactElement,
+    inline: boolean,
+    className: string,
+    children: ReactElement[]
+}
+
 const Blog = ({ blog, content }: Props) => {
+    SyntaxHighlighter.registerLanguage('jsx', jsx);
+    SyntaxHighlighter.registerLanguage('markdown', markdown);
+
     return (
         <Layout>
             <NextSeo 
@@ -48,9 +70,47 @@ const Blog = ({ blog, content }: Props) => {
             <p className='text-sm mb-16'>
                 {blog.attributes.date}
             </p>
-            <ReactMarkdown className='justify-between'  rehypePlugins={[rehypeRaw, rehypeHighlight]} >
+            {/* <ReactMarkdown components={{
+            code({ inline, className, children, ...props }: CodeProps) {
+              const match = /language-(\w+)/.exec(className || '')
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  children={String(children).replace(/\n$/, '')}
+                  language={'markdown'}
+                />
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              )
+            }}}>{content}</ReactMarkdown>
+            
+            // <SyntaxHighlighter language={'markdown'} style={materialDark}>
+            // {content}
+            // </SyntaxHighlighter> */}
+{/*             <SyntaxHighlighter language='javascript' style={materialDark}>
                 {content}
-            </ReactMarkdown>
+            </SyntaxHighlighter> */}
+            <ReactMarkdown
+          children={content}
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '')
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  children={String(children).replace(/\n$/, '')}
+                  language={match[1]}
+                  style={{ code: { fontFamily: 'inherit' }, ...materialDark }}
+                  {...props}
+                />
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              )
+            },
+          }}
+        /> 
             </div>
         </Layout>
     )
@@ -83,6 +143,5 @@ export async function getServerSideProps({ params }: ServerSideProps) {
         }
     }
 }
-
 
 export default Blog;
