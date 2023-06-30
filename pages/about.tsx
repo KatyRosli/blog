07674f37baa7d4150b4375d3581b6a6de3ee6332
Katy Rosli <katyrosli@gmail.com>
2023-06-30 14:59:16 +0200

    update work experiences

--- pages/about.tsx
+++ pages/about.tsx
@@ -14,16 +14,18 @@ export default function About() {
         husband and my son.</p>
         <h3>Education</h3>
         <p className='text-base mb-16'>I have a BA Hons in Communication Design from The Glasgow School of Art. I graduated back in 2014. 
-        I am currently working is a Frontend Developer.</p>
+        I am currently employed as a Frontend Developer.</p>
         <h3>Experiences</h3>
         <p className='text-base mb-16'>Over the years, I have worked for large organisations to start ups. 
         To name some, I’ve worked for Klarna, Braive, Hero Gaming, Asia Genomics and Sonoport. 
-        All of my past experiences can be seen <Link className='text-violet-700 underline' href='https://www.linkedin.com/in/katy-rosli-761b70185/'> here </Link> on my LinkedIn.</p>
+        I am currently employed as a Frontend Developer in Stockholm, Sweden. 
+        My work involves creating intutive and visually appealing user interfaces (UX and UI design) for web applications.
+        All of my current and past experiences can be seen <Link className='text-violet-700 underline' href='https://www.linkedin.com/in/katy-rosli-761b70185/'> here </Link> on my LinkedIn.</p>
         <h3>Skills and Expertise</h3>
         <p className='text-base'>Languages: TypeScript, JavaScript, HTML, CSS, SCSS.</p>
         <p className='text-base'>Frontend: React Js, Next Js, Redux, React Native, Vue Js, Angular, jQuery, Bootstrap, Tailwind.</p>
         <p className='text-base'>Backend: NodeJs, ExpressJs, Axios, Nodemon, REST, GraphQL, MongoDB, PostgresSQL, Strapi.</p>
-        <p className='text-base mb-16'>Test: Mocha, Jest.</p>
+        <p className='text-base mb-16'>Test: Mocha, Jest, Jasmine, Karma.</p>
         </div>
       </div>
     </Layout>
