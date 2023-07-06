149bcf912003a2a46c8c8f0efcd6341f0fbe883d
Katy Rosli <katyrosli@gmail.com>
2023-07-06 14:59:50 +0200

    implemented events on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,29 +1,59 @@
 import { BsLinkedin, BsGithub, BsBehance, BsInstagram } from 'react-icons/bs';
+import { event } from '../gtag';
 
 const Footer = () => {
     const openInNewTab = (url:string) => {
         window.open(url, '_blank', 'noreferrer');
     };
+
+    const trackSocialMediaClick = (platform:string) => {
+        event({
+            action: 'social_media_click',
+            category: 'Footer',
+            label: `Social Media: ${platform}`,
+        });
+    };
+
     return (
         <footer className='bg-violet-50 dark:bg-stone-900 w-full bottom-0 p-4 mt-24'>
-        <div className="w-full md:flex md:w-auto">
-        <ul className="justify-between px-4 mx-auto lg:max-w-7xl md:flex md:px-16">
-          <li className='pb-6 text-xl py-2 md:px-6 text-center border-b-2 md:border-b-0'>
-          <button><BsLinkedin onClick={() => openInNewTab('https://www.linkedin.com/in/katy-rosli-761b70185/')} /></button>
-          </li>
-          <li className='pb-6 text-xl py-2 md:px-6 text-center border-b-2 md:border-b-0'>
-          <button><BsGithub onClick={() => openInNewTab('https://github.com/KatyRosli')} /></button>
-          </li>
-          <li className='pb-6 text-xl py-2 md:px-6 text-center border-b-2 md:border-b-0'>
-          <button><BsBehance onClick={() => openInNewTab('https://www.behance.net/KatyLii')} /></button>
-          </li>
-          <li className='pb-6 text-xl py-2 md:px-6 text-center border-b-2 md:border-b-0'>
-          <button><BsInstagram onClick={() => openInNewTab('https://www.instagram.com/katy.rosli/')} /></button>
-          </li>
-        </ul>
-        </div>
-        <div className='justify-center mx-auto lg:max-w-7xl md:items-center md:flex md:px-16'>
-            <p className='text-center'>© 2023 Katy Rosli</p>
+            <div className="w-full md:flex md:w-auto">
+                <ul className="justify-between px-4 mx-auto lg:max-w-7xl md:flex md:px-16">
+                    <li className='pb-6 text-xl py-2 md:px-6 text-center border-b-2 md:border-b-0'>
+                        <button onClick={() => {
+                            openInNewTab('https://www.linkedin.com/in/katy-rosli-761b70185/');
+                            trackSocialMediaClick('Linkedin');
+                        }}>
+                            <BsLinkedin /> 
+                        </button>
+                    </li>
+                    <li className='pb-6 text-xl py-2 md:px-6 text-center border-b-2 md:border-b-0'>
+                        <button onClick={() => {
+                            openInNewTab('https://github.com/KatyRosli');
+                            trackSocialMediaClick('Github');
+                        }}>
+                            <BsGithub />
+                        </button>
+                    </li>
+                    <li className='pb-6 text-xl py-2 md:px-6 text-center border-b-2 md:border-b-0'>
+                        <button onClick={() => {
+                            openInNewTab('https://www.behance.net/KatyLii');
+                            trackSocialMediaClick('Behance');
+                        }}>
+                            <BsBehance />
+                        </button>
+                    </li>
+                    <li className='pb-6 text-xl py-2 md:px-6 text-center border-b-2 md:border-b-0'>
+                        <button onClick={() => {
+                            openInNewTab('https://www.instagram.com/katy.rosli/');
+                            trackSocialMediaClick('Instagram');
+                        }}>
+                            <BsInstagram />
+                        </button>
+                    </li>
+                </ul>
+            </div>
+            <div className='justify-center mx-auto lg:max-w-7xl md:items-center md:flex md:px-16'>
+                <p className='text-center'>© 2023 Katy Rosli</p>
             </div>
         </footer>
     )
